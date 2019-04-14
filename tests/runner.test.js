96506de76861d0611846a34f915d403b22654bab
Enrique Gonzalez <enrique@saucelabs.com>
2019-04-13 20:58:51 -0700

    add test for parentTunnel and update snapshot

--- tests/runner.test.js
+++ tests/runner.test.js
@@ -20,6 +20,25 @@ test('runPerformanceTest', async () => {
     expect(remote.mock.calls).toMatchSnapshot()
 })
 
+test('runPerformanceTest w/ parentTunnel', async () => {
+    const result = await runPerformanceTest(
+        'myuser',
+        'mykey',
+        {
+            region: 'eu',
+            platformName: 'Playstation',
+            browserVersion: 123,
+            tunnelIdentifier: 'foobar',
+            parentTunnel: 'foobaz'
+        },
+        'testname',
+        'buildname',
+        '/some/dir'
+    )
+    expect(result).toMatchSnapshot()
+    expect(remote.mock.calls).toMatchSnapshot()
+})
+
 test('runPerformanceTest without args', async () => {
     const result = await runPerformanceTest(
         'myuser',
